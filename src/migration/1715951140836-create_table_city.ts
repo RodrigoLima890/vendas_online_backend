7486c627f16738d2e9765fdc3afc90c0ce2ac03a
RodrigoLima890 <rodrigodaschagas1234@gmail.com>
2024-05-17 10:40:39 -0300

    feat: create migrations state, city and address

---
+++ src/migration/1715951140836-create_table_city.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateTableCity1715951140836 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+        CREATE TABLE public.city (
+            id integer NOT NULL PRIMARY KEY,
+            state_id integer NOT NULL,
+            name character varying NOT NULL,
+            created_at timestamp without time zone DEFAULT now() NOT NULL,
+            updated_at timestamp without time zone DEFAULT now() NOT NULL,
+            FOREIGN KEY (state_id) REFERENCES public.state(id)  -- Correção da palavra "foreign"
+        );
+        
+
+        CREATE SEQUENCE public.city_id_seq AS integer
+        START WITH 1 INCREMENT BY 1
+        NO MINVALUE
+        NO MAXVALUE
+        CACHE 1;
+
+        ALTER SEQUENCE public.city_id_seq OWNED BY public.city.id;
+        ALTER TABLE public.city ALTER COLUMN id SET DEFAULT nextval('public.city_id_seq'::regclass);
+        `)
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`DROP TABLE public.city;`)
+    }
+
+}
